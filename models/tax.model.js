1d531347f9dddbba2f8bc5269efc95808b52c012
Mirza Mohammad Azwad <50703605+mirzaazwad@users.noreply.github.com>
2023-11-03 10:59:02 +0600

    Models and Docker Compose

---
+++ models/tax.model.js
@@ -0,0 +1,27 @@
+
+const mongoose = require('mongoose');
+
+const taxSchema = new mongoose.Schema({
+  nid: {
+    type: String,
+    required: true,
+  },
+  year: {
+    type: Number,
+    required: true,
+  },
+  yearly_amount: {
+    type: Number,
+    required: true,
+  },
+  taxable_amount: {
+    type: Number,
+    required: true,
+  },
+});
+
+taxSchema.index({ nid: 1, year: 1 }, { unique: true });
+
+const Tax = mongoose.model('Tax', taxSchema);
+
+module.exports = Tax;
---
+++ models/user.model.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    nid: {
+        type: String,
+        required: true,
+        unique:true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    fullname: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: true,
+        enum: ['male', 'female']
+    },
+    
+    city_corporation: {
+        type: String,
+        required: true
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
