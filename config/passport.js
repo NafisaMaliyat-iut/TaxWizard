b629c85dc7d1ad0d5e096e722e33ce46f1d46643
NafisaMaliyat-iut <nafisamaliyat@iut-dhaka.edu>
2023-11-03 10:17:46 +0600

    configuring tailwind

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
-const Customer = require('../models/customer.model');
-const Manager = require('../models/manager.model');
+// const Customer = require('../models/customer.model');
+// const Manager = require('../models/manager.model');
 const { ExtractJwt } = require('passport-jwt');
 const JwtStrategy = require('passport-jwt').Strategy;
 
@@ -8,15 +8,15 @@ passport.use(new JwtStrategy({
   secretOrKey: process.env._JWT_SECRET,
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
 }, async (payload, done) => {
-  const customer = await Customer.findById(payload.sub);
-  const manager = await Manager.findById(payload.sub);
-  if (customer) {
-    done(null, customer);
-  } 
-  else if(manager){
-    done(null, manager);
-  }
-  else {
+  // const customer = await Customer.findById(payload.sub);
+  // const manager = await Manager.findById(payload.sub);
+  // if (customer) {
+  //   done(null, customer);
+  // } 
+  // else if(manager){
+  //   done(null, manager);
+  // }
+  // else {
     done(null, false);
-  }
+  // }
 }));
