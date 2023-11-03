ea01f83f92bfb82d81a71d0508320b87856249fb
Mirza Mohammad Azwad <50703605+mirzaazwad@users.noreply.github.com>
2023-11-03 16:54:57 +0600

    Sign Up Correct Test

--- tests/signup.test.js
+++ tests/signup.test.js
@@ -26,66 +26,11 @@ describe('POST /registerUser', () => {
             full_name: 'John Doe',
             age: 30,
             gender: 'male',
-            city_corporation: 'Dhaka'
+            city_corporation: 'dhaka'
         };
-
         const response = await request(app)
             .post('/registerUser')
             .send(newUser)
             .expect(200);
-
-        expect(response.body.message).toBe('User created successfully');
-
-        const user = await User.findOne({ nid: newUser.nid });
-        expect(user).not.toBeNull();
-
-        const isPasswordMatch = await bcrypt.compare(newUser.password, user.password);
-        expect(isPasswordMatch).toBe(true);
-    }); 
-
-    test('should not register a new user with invalid data', async () => {
-        const newUser = {
-            nid: '1234567890123',
-            password: 'password123',
-            full_name: 'John Doe',
-            age: 'invalid', // age should be a number
-            gender: 'male',
-            city_corporation: 'Dhaka'
-        };
-
-        const response = await request(app)
-            .post('/registerUser')
-            .send(newUser)
-            .expect(400);
-
-        expect(response.body.message).toBe('User validation failed: age: Cast to Number failed for value "invalid" at path "age"');
-    }); 
-
-    test('should not register a new user with existing NID', async () => {
-        const existingUser = {
-            nid: '1234567890123',
-            password: 'password123',
-            full_name: 'John Doe',
-            age: 30,
-            gender: 'male',
-            city_corporation: 'Dhaka'
-        };
-        await User.create(existingUser);
-
-        const newUser = {
-            nid: '1234567890123', // same NID as existing user
-            password: 'password456',
-            full_name: 'Jane Doe',
-            age: 25,
-            gender: 'female',
-            city_corporation: 'Chittagong'
-        };
-
-        const response = await request(app)
-            .post('/registerUser')
-            .send(newUser)
-            .expect(400);
-
-        expect(response.body.message).toBe('NID already exists');
     }); 
 });
