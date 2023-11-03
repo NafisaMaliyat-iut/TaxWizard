1f61b19b478674c800ace2e7d3de48434cba85ee
Mirza Mohammad Azwad <50703605+mirzaazwad@users.noreply.github.com>
2023-11-03 19:34:29 +0600

    Server Update

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const connectToDatabase = require("./config/db_connection");
 const app= require("./app")
 connectToDatabase(process.env._MONGO_URI,process.env._NODE_ENV);
 const server=app.listen(process.env._PORT);
-
+console.log(process.env._MONGO_URI);
 module.exports = server;
