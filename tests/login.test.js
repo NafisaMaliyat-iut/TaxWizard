97ae16abbf38a9e85879f57ed89ac777ffc7a48f
Tahlil_Mahfuz <tahlilkfaiyaz@gmail.com>
2023-11-03 11:37:54 +0600

    Added Login test

---
+++ tests/login.test.js
@@ -0,0 +1,37 @@
+const request = require('supertest');
+const app = require('../app');
+
+describe('POST /login', () => {
+    it('should return a 200 status code and a success message when valid email and password are provided', async () => {
+        const res = await request(app)
+            .post('/login')
+            .send({
+                email: 'testuser@example.com',
+                password: 'testpassword'
+            });
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.message).toEqual('Login successful');
+    });
+
+    it('should return a 401 status code and an error message when invalid email is provided', async () => {
+        const res = await request(app)
+            .post('/login')
+            .send({
+                email: 'invalidemail',
+                password: 'testpassword'
+            });
+        expect(res.statusCode).toEqual(401);
+        expect(res.body.message).toEqual('Invalid email or password');
+    });
+
+    it('should return a 401 status code and an error message when invalid password is provided', async () => {
+        const res = await request(app)
+            .post('/login')
+            .send({
+                email: 'testuser@example.com',
+                password: 'invalidpassword'
+            });
+        expect(res.statusCode).toEqual(401);
+        expect(res.body.message).toEqual('Invalid email or password');
+    });
+});
