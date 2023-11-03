2855e6d3d97eb4986dcb65aa9b67bd7a8d70661b
Tahlil_Mahfuz <tahlilkfaiyaz@gmail.com>
2023-11-03 19:02:17 +0600

    modifying calculate tax

--- routes/tax.routes.js
+++ routes/tax.routes.js
@@ -3,10 +3,11 @@ const route = require("express").Router();
 const passport = require("passport");
 const { getprofile } = require("../controllers/profile.controllers.js");
 const {
-  getHomePage, getCalculateTaxPage,
+  getHomePage, getCalculateTaxPage, postCalculateTax,
 } = require("../controllers/tax.controllers.js");
 
 route.get("/home", getHomePage);
 route.get("/profile-information",passport.authenticate('jwt',{session:false}),getprofile);
 route.get("/calculate", getCalculateTaxPage);
+route.post("/calculateTax",passport.authenticate('jwt',{session:false}), postCalculateTax);
 module.exports = route;
