e1ba81b1db566f649f35720f231b43aa92f0e1ff
Tahlil_Mahfuz <tahlilkfaiyaz@gmail.com>
2023-11-03 11:45:52 +0600

    Modified Public controllers

--- controllers/public.controllers.js
+++ controllers/public.controllers.js
@@ -14,19 +14,7 @@ const welcome=async(req,res)=>{
     }
 }
 
-//get register page
-const getRegister=async(req,res)=>{
-    try{
-        return res.status(200).render('pages/register');
-    }
-    catch(error){
-        return res.status(404).render('error404');
-    }
-}
-
-
 //Export modules
 module.exports={
-    getRegister,
     welcome
 }
